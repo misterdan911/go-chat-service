2cf1ef4ddd8415cec507603a2293bd13cbc1ddc8
Danu <dciptadi@gmail.com>
2024-08-07 10:47:27 +0700

    update DB connection code

--- db/database.go
+++ db/database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,9 +17,16 @@ var ctx context.Context
 func ConnectDatabase() {
 
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-	//serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	//opts := options.Client().ApplyURI("mongodb+srv://misterdan:M1zt3rD4nz212@cluster0.n5fdxh4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
-	opts := options.Client().ApplyURI("mongodb://localhost:27017")
+	var opts *options.ClientOptions
+	useMongodbAtlas := os.Getenv("USE_MONGODB_ATLAS")
+
+	if useMongodbAtlas == "true" {
+		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+		opts = options.Client().ApplyURI("mongodb+srv://misterdan:M1zt3rD4nz212@cluster0.n5fdxh4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
+	} else {
+		opts = options.Client().ApplyURI("mongodb://localhost:27017")
+	}
+
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
